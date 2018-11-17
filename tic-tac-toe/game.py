d352b05c206e270dd04537136a0071732bf2dbfa
Daniele Agueci <d.agueci1@gmail.com>
2018-11-17 14:55:50 -0500

    Add function to pick player symbol

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -1,3 +1,28 @@
+inputs = { '1': ' ',  '2': ' ', '3': ' ', '4': ' ', '5': ' ', '6': ' ', '7': ' ', '8': ' ', '9': ' ' }
+
+players = { '1': '', '2': '' }
+
+
+def player_pick_symbol():
+    symbol = ''
+
+    while symbol == '':
+        symbol = input('Player 1 select your symbol (x/o): ')
+
+        if symbol == 'x':
+            players['1'] = 'x'
+            players['2'] = 'o'
+        elif symbol == 'o':
+            players['1'] = 'o'
+            players['2'] = 'x'
+        else:
+            print('You entered an invalid symbol! Please select "x" or "o"')
+            symbol = ''
+
+    print(f'Player 1 is {players["1"]}')
+    print(f'Player 2 is {players["2"]}')
+        
+
 def print_board_configuration():
     print('     |     |     ')
     print('  7  |  8  |  9  ')
@@ -24,8 +49,7 @@ def print_board(inputs):
     print('  {}  |  {}  |  {}  '.format(inputs['1'], inputs['2'], inputs['3']))
     print('     |     |     ')
 
-inputs = { '1': ' ',  '2': ' ', '3': ' ', '4': ' ', '5': ' ', '6': ' ', '7': ' ', '8': ' ', '9': ' ' }
-
-print_board_configuration()
-print_board(inputs)
+player_pick_symbol()
+#print_board_configuration()
+#print_board(inputs)
 
