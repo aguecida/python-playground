285ed51304e6af5452fc5effe800f8ba01e85e7f
Daniele Agueci <d.agueci1@gmail.com>
2018-11-18 18:55:23 -0500

    Add player loop

--- blackjack/game.py
+++ blackjack/game.py
@@ -31,8 +31,41 @@ def display_hands(player, dealer):
     player.display_hand()
     dealer.display_hand()
 
+def get_player_command():
+    while True:
+        command = input('\nHit or Stay?: ')
+        if command.lower() in ['h','hit']:
+            return 'hit'
+        elif command.lower() in ['s','stay']:
+            return 'stay'
+        else:
+            print('\nSorry, I did not recognize that command. Please try again.')
+
 if __name__ == '__main__':
     player, dealer = create_players()
     deck = new_deck()
     deal_cards(deck, player, dealer)
     display_hands(player, dealer)
+
+    # Player's turn
+    while True:
+        player_command = get_player_command()
+        if player_command == 'hit':
+            card = deck.deal_card()
+            player.add_card_to_hand(card)
+            if player.is_bust() or player.has_blackjack():
+                break
+            else:
+                display_hands(player, dealer)
+        else: # Stay
+            break
+
+    if player.is_bust():
+        print('\nYou went over 21. Dealer wins.')
+        quit()
+
+    if player.has_blackjack():
+        print('\nYou got 21! You win!')
+        quit()
+
+
