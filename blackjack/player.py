1147b0326aed121d9a419c8a58919e8b76144213
Daniele Agueci <d.agueci1@gmail.com>
2018-11-18 17:25:01 -0500

    Create suit enum and add player methods

--- blackjack/player.py
+++ blackjack/player.py
@@ -3,3 +3,15 @@ class Player():
 
     def __init__(self, name = 'Dealer'):
         self.name = name
+
+    def total(self):
+        total = 0
+        for card in self.cards:
+            total += card.value
+        return total
+
+    def is_bust(self):
+        return self.total() > 21
+
+    def has_blackjack(self):
+        return self.total() == 21
